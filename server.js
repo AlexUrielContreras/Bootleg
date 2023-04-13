d46a58fbdc58c9c9e377127ef6f5b531508719b7
Alex Contreras <alex.code29@gmail.com>
2023-04-13 17:34:18 -0500

    fix: eslint changes

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
 const path = require('path');
 const express = require('express');
-const fileUpload=require("express-fileupload")
-const bodyParser = require("body-parser")
+const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({
-    helpers
+	helpers,
 });
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: process.env.DB_SECRET,
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize,
-        checkExpirationInterval: 1000 * 60 * 10, // will check every 10 minutes
-        expiration: 1000 * 60 * 30 // will expire after 30 minutes
-    })
+	secret: process.env.DB_SECRET,
+	cookie: {},
+	resave: false,
+	saveUninitialized: true,
+	store: new SequelizeStore({
+		db: sequelize,
+		checkExpirationInterval: 1000 * 60 * 10, // will check every 10 minutes
+		expiration: 1000 * 60 * 30, // will expire after 30 minutes
+	}),
 };
 
 const app = express();
@@ -31,14 +31,14 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(session(sess));
-app.use(fileUpload())
+app.use(fileUpload());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(routes);
 
 sequelize.sync();
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`Now listening`));
-  });
+	app.listen(PORT, () => console.log(`Now listening`));
+});
